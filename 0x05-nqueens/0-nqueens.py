0d8dcd69a2646597ade3ea7b6f57beccd5a32733
James Alhassan <jamesalhassan052@gmail.com>
2023-08-04 05:18:20 +0000

    Update 0-nqueens -- Add queens()

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -16,3 +16,13 @@ if int(sys.argv[1]) < 4:
     exit(1)
 
 n = int(sys.argv[1])
+
+
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
